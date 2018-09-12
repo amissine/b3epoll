9a6edf18266ce6e275279d54fd907264aea838cc
Alec Missine <alec_missine@yahoo.com>
2018-09-12 18:14:49 -0400

    Add the addon

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 
-// const addon = require('bindings')('addon')
+const addon = require('bindings')('addon')
 var b3Count = 0
+var theItem
 
 class B3 {
   constructor () {
@@ -9,7 +10,17 @@ class B3 {
       throw new Error('Multiple instances of B3 are not allowed')
     }
   }
-  done () {
+  start () {
+    var result = addon.start((item, thePrime) => {
+      console.log('thePrime: %d', thePrime)
+      theItem = item
+    })
+    console.log('start result: ' + result)
+    return true
+  }
+  stop () {
+    var result = addon.stop(theItem, false)
+    console.log('stop result: ' + result)
     return true
   }
 }
--- test/t01.js
+++ test/t01.js
@@ -6,14 +6,23 @@ const assert = require('assert-plus')
 const B3 = require('../')
 var b3 = new B3()
 
-describe('Basic B3 functionality', () => {
+describe('Basic B3 functionality:', () => {
   it('does not require before(...) to get instantiated', done => {
     assert.object(b3, 'b3')
-    assert.ok(b3.done(), 'b3.done()')
     done()
   })
   it('allows exactly one singleton instance of itself', done => {
     assert.throws(() => { var x = new B3(); return x }, 'Must throw Error')
     done()
   })
+  it('starts the addon with consumer and producer threads', done => {
+    assert.ok(b3.start(), 'b3.start()')
+    done()
+  })
+  it('stops the addon and cleans up', done => {
+    setTimeout(() => {
+      assert.ok(b3.stop(), 'b3.stop()')
+      done()
+    }, 5000)
+  }).timeout(6000)
 })
