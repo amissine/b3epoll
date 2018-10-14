70c6a2fb61f2c24bd47e410ef8211b5cc37d688c
Alec Missine <alec_missine@yahoo.com>
2018-10-14 17:23:54 -0400

    Debug handleBackpressure test

--- debug.js
+++ debug.js
@@ -1,7 +1,40 @@
 'use strict'
 
-const B3 = require('./')
-const b3 = new B3()
+const B3 = require('./b3')
 
-b3.start()
-setTimeout(() => b3.stop(), 5000)
+handleBackpressure(() => console.log('done'))
+
+function handleBackpressure (done) {
+  var b3 = b3common('handleBackpressure', 20, done)
+  var count = 8
+  b3.open()
+  while (count--) b3.b2lrProducer.send(`+${B3.timeMs()} ms handleBackpressure`)
+  setTimeout(() => b3.close(), 50)
+}
+
+function b3common (functionName, timeoutMs, done) {
+  var b3 = new B3({ noSelfTest: true, noDefaultListeners: true })
+  var count = 2
+  b3.b2lrConsumer.on('token', t => {
+    setTimeout(() => {
+      b3.b2rlProducer.send(`+${B3.timeMs()} ms echo "${t.message}" back`)
+      b3.b2lrConsumer.doneWith(t)
+    }, timeoutMs)
+  })
+  b3.b2rlConsumer.on('token', t => {
+    console.log('+%d ms - consumer sid %d, token sid %d, message %s, delay %d µs',
+      B3.timeMs(), b3.b2rlConsumer.sid, t.sid, t.message, t.delay)
+    b3.b2rlConsumer.doneWith(t)
+  })
+  b3.b2lrConsumer.on('close', () => {
+    if (--count) return
+    console.log(`+${B3.timeMs()} ms ${functionName} done`)
+    done()
+  })
+  b3.b2rlConsumer.on('close', () => {
+    if (--count) return
+    console.log(`+${B3.timeMs()} ms ${functionName} done`)
+    done()
+  })
+  return b3
+}
